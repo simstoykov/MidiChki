320f43fb46212bc5b809082c2b1ac864ffda8925
Simeon Stoykov <ss2476@cam.ac.uk>
2020-05-29 18:11:42 +0100

    Changing for Yamaha...

--- listener/playing.py
+++ listener/playing.py
@@ -2,9 +2,12 @@ import logging
 import time
 import explorerhat as eh
 import pygame.midi
+import argparse
+
 from random import random
 from concurrent.futures import ProcessPoolExecutor
 from requests_futures.sessions import FuturesSession
+
 
 session = FuturesSession(executor=ProcessPoolExecutor(max_workers=3))
 logging.basicConfig(level=logging.INFO)
@@ -14,7 +17,7 @@ def random_between(start, to):
     return int(start + random() * (to - start))
 
 
-def find_roland_id():
+def find_piano_id(device_name):
     pygame.midi.quit()
     pygame.midi.init()
 
@@ -28,24 +31,24 @@ def find_roland_id():
         is_input = info[2]
 
         logging.info(f"Checking {name}, is_input={is_input}...")
-        if is_input and "Roland" in name:
+        if is_input and device_name in name:
             print(f"Found it on id {i}!")
             return i
 
-    logging.warning("Couldn't find Roland...")
-    raise FileNotFoundError("Couldn't find Roland...")
+    logging.warning("Couldn't find piano...")
+    raise FileNotFoundError("Couldn't find piano...")
 
 
-def wait_for_roland():
+def wait_for_piano(device_name):
     while True:
         try:
-            roland_id = find_roland_id()
+            piano_id = find_piano_id(device_name)
             break
         except FileNotFoundError as e:
             logging.info("Retrying...")
             time.sleep(1)
 
-    return pygame.midi.Input(roland_id)
+    return pygame.midi.Input(piano_id)
 
 
 def handle_note_pressed(note, velocity):
@@ -66,7 +69,7 @@ def read_to_string(read, cur_time):
 
 
 # write_path = "/media/pi/SS Backup/midichki/simefile.txt"
-write_path = "/home/pi/Documents/Developing/MidiChki/dontgitit/simefile.txt"
+write_path = "/home/pi/Developing/MidiChki/dontgitit/simefile.txt" # TODO: Make a cli argument
 def persist_stuff(strval):
     with open(write_path, 'a+') as the_file:
         the_file.write(strval + "\n")
@@ -76,6 +79,7 @@ upload_url = "https://mighty-island-21925.herokuapp.com/postNotes"
 # upload_url = "http://192.168.43.223:8080/postNotes"
 def upload_stuff(reads):
     logging.info(f"Uploading {len(reads)} values to {upload_url}")
+    logging.info(reads)
     session.post(upload_url, json=reads)
 
 
@@ -85,6 +89,10 @@ def midi_event(read, cur_time):
 
     if status == 144:
         handle_note_pressed(note, velocity)
+
+    if status != 248: # This is a midi clock which we dislike
+        to_upload = read[0] + [read[1], cur_time]
+        upload_stuff(to_upload)
 
     strval = read_to_string(read, cur_time)
     persist_stuff(strval)
@@ -97,13 +105,18 @@ rotating = 0
 if __name__ == '__main__':
     logging.info("Hello!")
 
-    roland = wait_for_roland()
+    parser = argparse.ArgumentParser(description='Stream Midichki to the server')
+    parser.add_argument('-p', '--piano', required=True)
+    args = parser.parse_args()
+
+    device_name = args.piano
+    piano = wait_for_piano(device_name)
 
     last_note_time = time.time()
 
     logging.info("Indefinitely listening for notes...")
     while True:
-        reads = roland.read(100)
+        reads = piano.read(100)
         # reads = [[[144, 2, 3, 4], 5]]
         time.sleep(1)
         logging.info(f"Read {len(reads)} stuff")
@@ -114,17 +127,15 @@ if __name__ == '__main__':
             elapsed = cur_time - last_note_time
             if elapsed > 5:
                 try:
-                    roland.close()
+                    piano.close()
                 except Exception:
-                    logging.info("Roland was indeed detached")
+                    logging.info("piano was indeed detached")
                     raise Exception("Exiting... Start me again!")
 
-                roland = wait_for_roland()
+                piano = wait_for_piano()
                 last_note_time = cur_time
         else:
             last_note_time = cur_time
             for read in reads:
                 midi_event(read, cur_time)
-
-            upload_stuff(list(map(lambda read: read[0] + [read[1], cur_time], reads)))
 
